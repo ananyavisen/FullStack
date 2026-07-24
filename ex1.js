393a11ac33836e5fed7f09ebfc5ffa9bec310d1e
Ananya Visen <ananvisen@gmail.com>
2026-07-24 10:28:49 +0530

    Stream Api - Filter

--- ex1.js
+++ ex1.js
@@ -1,3 +1,3 @@
 let a = 10;
-let a = 30;
+let b = 30;
 console.log("a :  ", a);
---
+++ ex3.js
@@ -0,0 +1,6 @@
+const a=[1,2,3,4,5,6,7,8,9]
+const even=a.filter((i)=>i%2==0)
+console.log("a: ",a);
+console.log("even values: ",even);
+const square = a.filter((i)=>i*i);
+console.log("Square: ", square);
