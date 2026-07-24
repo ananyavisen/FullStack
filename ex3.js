479f6d480484042300b3d5871348cfdd3cbdb250
Ananya Visen <ananvisen@gmail.com>
2026-07-24 10:32:41 +0530

    Stream API

--- ex3.js
+++ ex3.js
@@ -2,5 +2,7 @@ const a=[1,2,3,4,5,6,7,8,9]
 const even=a.filter((i)=>i%2==0)
 console.log("a: ",a);
 console.log("even values: ",even);
-const square = a.filter((i)=>i*i);
+const square = a.filter((i)=>(i*i));
 console.log("Square: ", square);
+const sum = square.reduce((i,s)=>(i+s))
+console.log("sum: ", sum);
