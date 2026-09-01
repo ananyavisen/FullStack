af3462956002c87b45014e4f92881fec23eeaa60
Ananya Visen <ananvisen@gmail.com>
2026-09-01 14:48:19 +0530

    Book Store using npm

---
+++ Book-npm-app/App.jsx
@@ -0,0 +1,13 @@
+import Book from "./Book";
+
+function App(){
+    return(
+        <div>
+            <h1><center>My Book Store</center></h1>
+            <div>
+                <Book />
+            </div>
+        </div>
+    )
+}
+export default App;
---
+++ Book-npm-app/Book.jsx
@@ -0,0 +1,11 @@
+function Book(){
+    return(
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsiix0gdWY_Cp8e69K9Q7oD9luodHRqDDabwoJII715w&s=10" width={180} height={200} alt="Book Image" />
+            <h2>Title: ReactJS Book</h2>
+            <h2>Price: ₹465</h2>
+            <button> Add to Cart </button>
+        </div>
+    )
+}
+export default Book;
