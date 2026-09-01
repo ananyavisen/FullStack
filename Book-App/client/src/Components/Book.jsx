f92eff7556de835d95ae1b5719166ed6ff237122
Ananya Visen <ananvisen@gmail.com>
2026-09-01 15:43:18 +0530

    Added Dynamic Data

--- Book-App/client/src/Components/Book.jsx
+++ Book-App/client/src/Components/Book.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Book = () => {
+const Book = ({props}) => {
+    
   return (
    <div className='book'>
-    <img src='https://canducci.gallerycdn.vsassets.io/extensions/canducci/create-reactjs-components/1.2.3/1600731009124/Microsoft.VisualStudio.Services.Icons.Default' width={120} height={120} alt='Book Image' />
-        <h2>Title: ReactJS</h2><br />
-        <h2>Price: ₹567</h2><br />
+    <img src={props.image} width={120} height={120} alt='Book Image' />
+        <h2>Title: {props.title}</h2><br />
+        <h2>Price: ₹{props.price}</h2><br />
         <button> Add to Cart</button><br />
    </div>
   )
--- Book-App/client/src/Components/Bookstore.jsx
+++ Book-App/client/src/Components/Bookstore.jsx
@@ -2,12 +2,16 @@ import Book from "./Book"
 
 
 const Bookstore = () => {
+    const bookData = [
+    {image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsiix0gdWY_Cp8e69K9Q7oD9luodHRqDDabwoJII715w&s=10", title: "ReactJS", price: "465" },
+    {image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxf1kOGA1WfxBxUmIkbM7nE-TP7rpUkKNL5nOMlLNItg&s=10", title: "NodeJS", price: "500" },
+    {image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgS-IRlKHegMxrslY9XVulixMgx1PyT1tWO6ajR--pcA&s=10", title: "ExpressJS", price: "385" },
+];
   return (
     <div className="bookstore">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        {bookData.map((book,i)=> {
+            return <Book key={i} props={book} />;
+        })}
     </div>
   )
 }
